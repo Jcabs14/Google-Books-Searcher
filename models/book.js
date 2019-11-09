d075b36c83acaeaf2c6174999888de65870c141c
Jared Cabanilla <Jcabanilla@gmail.com>
2019-11-09 11:55:47 -0800

    fixed file structure

--- models/book.js
+++ models/book.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: { type: [String], required: true },
   description: String,
-  image: {},
+  image: {type: String, required: true},
   link:{type:String}
 });
 
